65ef142c9cfe6f1dadaf2c827248a828ec3c0960
Abdul Munim <farzimunim@gmail.com>
2020-09-24 05:33:11 -0700

    added auth screen

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
-import { AsyncStorage, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { AsyncStorage, View, Alert } from "react-native";
 import firebase from "react-native-firebase";
+import { WebView } from "react-native-webview";
+const host = "https://92a17d93fff6.ngrok.io";
 
-export default class App extends Component {
-  async componentDidMount() {
-    this.checkPermission();
-    this.createNotificationListeners();
-  }
+export default function App() {
+  const [uri, setUri] = useState(host);
+  useEffect(() => {
+    checkPermission();
+    createNotificationListeners();
+  }, []);
 
   //1
-  async checkPermission() {
+  async function checkPermission() {
     const enabled = await firebase.messaging().hasPermission();
     if (enabled) {
-      this.getToken();
+      getToken();
     } else {
-      this.requestPermission();
+      requestPermission();
     }
   }
 
   //3
-  async getToken() {
+  async function getToken() {
     let fcmToken = await AsyncStorage.getItem("fcmToken");
     if (!fcmToken) {
       fcmToken = await firebase.messaging().getToken();
@@ -31,32 +34,32 @@ export default class App extends Component {
   }
 
   //2
-  async requestPermission() {
+  async function requestPermission() {
     try {
       await firebase.messaging().requestPermission();
       // User has authorised
-      this.getToken();
+      getToken();
     } catch (error) {
       // User has rejected permissions
       console.log("permission rejected");
     }
   }
 
-  async createNotificationListeners() {
+  async function createNotificationListeners() {
     // This listener triggered when notification has been received in foreground
-    this.notificationListener = firebase
+    notificationListener = firebase
       .notifications()
       .onNotification((notification) => {
         const { title, body } = notification;
-        this.displayNotification(title, body);
+        displayNotification(title, body);
       });
 
     // This listener triggered when app is in backgound and we click, tapped and opened notifiaction
-    this.notificationOpenedListener = firebase
+    notificationOpenedListener = firebase
       .notifications()
       .onNotificationOpened((notificationOpen) => {
         const { title, body } = notificationOpen.notification;
-        this.displayNotification(title, body);
+        displayNotification(title, body);
       });
 
     // This listener triggered when app is closed and we click,tapped and opened notification
@@ -65,12 +68,11 @@ export default class App extends Component {
       .getInitialNotification();
     if (notificationOpen) {
       const { title, body } = notificationOpen.notification;
-      this.displayNotification(title, body);
+      displayNotification(title, body);
     }
   }
 
-  displayNotification(title, body) {
-    // we display notification in alert box with title and body
+  function displayNotification(title, body) {
     Alert.alert(
       title,
       body,
@@ -79,11 +81,28 @@ export default class App extends Component {
     );
   }
 
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Text>Welcome to React Native!</Text>
-      </View>
+  async function navigationStateChangeHandler(state) {
+    const isAuthRoutes = ["sign_in", "sign_up"].some((route) =>
+      state?.url.includes(route)
     );
+
+    const fcmToken = await AsyncStorage.getItem("fcmToken");
+
+    fcmToken &&
+      isAuthRoutes &&
+      setUri(`${host}/users/save_token?token=${fcmToken}`);
   }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <WebView
+        startInLoadingState={true}
+        source={{
+          uri: uri,
+        }}
+        onNavigationStateChange={navigationStateChangeHandler}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
 }
