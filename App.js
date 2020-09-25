e785cb78e0c805f397d5dcd96e491a19808efe68
Abdul Munim <farzimunim@gmail.com>
2020-09-25 08:54:28 -0700

    Fixed uri for webview

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AsyncStorage, View } from "react-native";
 import firebase from "react-native-firebase";
 import { WebView } from "react-native-webview";
-const host = "https://92a17d93fff6.ngrok.io";
+const host = "https://dc2641d4e4e0.ngrok.io";
 
 export default function App() {
   const [uri, setUri] = useState(host);
@@ -24,7 +24,7 @@ export default function App() {
     if (!fcmToken) {
       fcmToken = await firebase.messaging().getToken();
       if (fcmToken) {
-        // user has a device token
+        await AsyncStorage.removeItem("fcmToken");
         await AsyncStorage.setItem("fcmToken", fcmToken);
       }
     }
@@ -33,36 +33,32 @@ export default function App() {
   async function requestPermission() {
     try {
       await firebase.messaging().requestPermission();
-      // User has authorised
       getToken();
     } catch (error) {
-      // User has rejected permissions
       console.log("permission rejected");
     }
   }
 
   async function navigationStateChangeHandler(state) {
     const isAuthRoutes = ["sign_in", "sign_up"].some((route) =>
-      state?.url.includes(route)
+      state.url.includes(route)
     );
-
     const fcmToken = await AsyncStorage.getItem("fcmToken");
-
-    fcmToken &&
-      isAuthRoutes &&
+    if(!!fcmToken && !isAuthRoutes){
       setUri(`${host}/users/save_token?token=${fcmToken}`);
+    }
+    if(isAuthRoutes){
+      setUri(state.url)
+    }
   }
 
   return (
-    <View style={{ flex: 1 }}>
       <WebView
-        startInLoadingState={true}
         source={{
           uri: uri,
         }}
         onNavigationStateChange={navigationStateChangeHandler}
         style={{ flex: 1 }}
       />
-    </View>
   );
 }
